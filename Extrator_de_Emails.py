f522409c20aed0286f044ea16a6728dbf80240cf
MiguelBragaGarcia <52131821+MiguelBragaGarcia@users.noreply.github.com>
2019-11-26 17:39:34 -0300

    Algumas melhorias
    Melhorando o código em questão da legibilidade.

--- Extrator_de_Emails.py
+++ Extrator_de_Emails.py
@@ -5,10 +5,10 @@ import pytesseract # Módulo para a utilização da tecnologia OCR
 
 #Caso tiver utilizando windows terá que passar onde o arquivo pytesseract.exe está em seu computador para que ele possa executar corretamente
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-
+LocalDosArquivos = "C:\\Users\\Nome do Usuário\\Desktop\\Teste de projeto"
 
 #Esta variável recebe todos os nomes das imagens, a partir do caminho da pastas para fazer a automatização da estração dos textos
-nomearquivo = os.listdir(" EX: C:\\Users\\nome do usuário\\Desktop\\Nova pasta\\Imagens")
+NomesdasImagens = os.listdir(LocalDosArquivos+"\\Imagens")
 
 
 #-----------------------------Funções----------------------------------------------------#
@@ -27,17 +27,13 @@ def temponto(dado):
 
 #----------------------------------------------------------------------------------------#
 
-for P in range(len(nomearquivo)):
- Dados= pytesseract.image_to_string( Image.open(" EX: C:\\Users\\nome do usuário\\Desktop\\Nova pasta\\Imagens\\"+str(nomearquivo[P])) )  # Extraindo o texto da imagem
+for P in range(len(NomesdasImagens)):
+ Dados= pytesseract.image_to_string( Image.open(LocalDosArquivos+"\\Imagens\\"+str(NomesdasImagens[P])) )  # Extraindo o texto da imagem
  Dados = Dados.split() #transformando os textos em listas
 
  for I in range(len(Dados)):
          if (("@" in Dados[I]) and (primeiraposicao(Dados[I])) and (temponto(Dados[I])) ):
-              arq=open ("EX: C:\\Users\\nome do usuário\\Desktop\\Nova pasta\\E-mail.txt",'a')
+              arq=open (LocalDosArquivos+"\\E-mail.txt",'a')
               arq.write(Dados[I]+"\n")
               arq.close()
               print(Dados[I])
-
-
-          
-
